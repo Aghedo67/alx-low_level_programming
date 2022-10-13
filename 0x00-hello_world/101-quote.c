17be4c34aa73a93d6ce912b1510f7c1eec12dfee
Emmanuel <aghedoemmanuel67@yahoo.com>
2022-10-13 15:21:49 -0700

    like me

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unitd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and hta piec e of art is useful\" - Dor Korpor, 2015-10-19\n", 59);
+	return (1);
+}
