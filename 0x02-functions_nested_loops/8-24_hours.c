33be88a6eb1a2bb88bb34400f9620aa55367a90c
Emmanuel <aghedoemmanuel67@yahoo.com>
2022-10-25 08:44:01 -0700

    8-24_hours.c task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,63 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * checkInDay - checks if hours given are inside a day
+ * @hour1: first digit of hour
+ * @hour2: second digit of hour
+ * Description: checks if hours given are inside a day
+ * Return: boolean
+ */
+
+int checkInDay(char hour1, char hour2)
+{
+	if (hour1 == '2' && hour2 == '4')
+	{
+		return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * jack_bauer - prints all minutes in a day using character like a nomad
+ * Description: print all minutes in a day
+ * Return: void
  */
 
 void jack_bauer(void)
 {
+	char hour1 = '0';
+	char hour2 = '0';
+	char minute1 = '0';
+	char minute2 = '0';
 
-	int hour, minute;
-
-
-	for (hour = 0; hour <= 23; hour++);
+	while (checkInDay(hour1, hour2))
 	{
-		for (minute = 0; minute <= 59; minute++)
+		_putchar(hour1);
+		_putchar(hour2);
+		_putchar(':');
+		_putchar(minute1);
+		_putchar(minute2);
+		_putchar('\n');
+		if (hour2 == '9' && (minute1 == '5' && minute2 == '9'))
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			hour1++;
+			hour2 = minute1 = minute2 = '0';
+		}
+		else if (minute1 == '5' && minute2 == '9')
+		{
+			hour2++;
+			minute1 = minute2 = '0';
+		}
+		else if (minute2 == '9')
+		{
+			minute1++;
+			minute2 = '0';
+		}
+		else
+		{
+			minute2++;
 		}
 	}
 }
