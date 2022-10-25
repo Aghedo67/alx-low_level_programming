15283fdc109b4f90de62a9e1df5540b36f224d26
Emmanuel <aghedoemmanuel67@yahoo.com>
2022-10-25 08:17:43 -0700

    7-print_last_digit.c task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit
- * @n: the number
- * Return: value of the last digit
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
  */
-int print_last_digit(int n)
+void print_diagonal(int n)
 {
-	int ld = n % 10;
+	int len, space;
 
-	if (ld < 10)
-		ld *= -1;
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
 
-	_putchar(ld + '0');
+			if (len == n - 1)
+				continue;
 
-	return (0);
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
 }
